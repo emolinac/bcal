c88a54780316b0d3af8ef0a129d8dcb37d2b17eb
bleak.twig <bruno.benkel@gmail.com>
2021-03-17 11:25:42 -0300

    Added a nice and comfy run script

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
 #!/bin/csh
+# Set filename.
+set nrows="7" # Set by hand based on bcal_geometry!
+set ncols="7" # Set by hand based on bcal_geometry!
+set FNAME="bcal_"`date +"%Y%m%d%H%M%S"`"_r"$nrows"c"$ncols".txt"
+
+# Run gemc.
+cd gemc/
+gemc bcal.gcard
+cd ../out/
+mv "output.txt" $FNAME
